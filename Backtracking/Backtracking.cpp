4698502423f2ace672f785ed00f620af870e8956
PERCY99 <43267172+PERCY99@users.noreply.github.com>
2020-04-28 23:36:44 +0530

    Update Backtracking.cpp

--- Backtracking/Backtracking.cpp
+++ Backtracking/Backtracking.cpp
@@ -1,15 +1,3 @@
-//Backtracking
-
-//Author: Abhishek Kumar Suman
-//Roll number:181210002
-//Branch: CSE 2nd year
-
-/*Rat in a Maze
-A Maze is given as N*N binary matrix of blocks where source block is the upper left most
-block i.e., maze[0][0] and destination block is lower rightmost block i.e., maze[N-1][N-1]. A
-rat starts from source and has to reach the destination. The rat can move only in two
-directions: forward and down. In the maze matrix, 0 means the block is a dead end and 1 means 
-the block can be used in the path from source to destination.*/
 
 /*Input : {1, 0, 0, 0}
           {1, 1, 0, 1}
