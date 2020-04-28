60688085681ac88cc130e8fd0603cf21e2d78be7
PERCY99 <43267172+PERCY99@users.noreply.github.com>
2020-04-28 23:40:17 +0530

    Create Bfs.cpp

---
+++ Graphs/Bfs.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include<queue>
+using namespace std;
+
+class Edge
+{
+public:
+    int v = 0;
+    int w = 0;
+
+    Edge(int v, int w)
+    {
+        this->v = v;
+        this->w = w;
+    }
+};
+
+int n = 7;
+vector<vector<Edge *>> graph(n, vector<Edge *>());
+
+void display()
+{
+    for (int i = 0; i < graph.size(); i++)
+    {
+        cout << i << " -> ";
+        for (Edge *e : graph[i])
+        {
+            cout << "(" << e->v << ", " << e->w << ") ";
+        }
+        cout << endl;
+    }
+}
+
+void addEdge(int u, int v, int w)
+{
+    if (u < 0 || v < 0 || u >= n || v >= n)
+        return;
+
+    graph[u].push_back(new Edge(v, w));
+    graph[v].push_back(new Edge(u, w));
+}
+
+void constructGraph()
+{
+    addEdge(0, 1, 10);
+    addEdge(0, 3, 10);
+    addEdge(1, 2, 10);
+    addEdge(2, 3, 40);
+    addEdge(3, 4, 2);
+    addEdge(4, 5, 2);
+    addEdge(4, 6, 3);
+    addEdge(5, 6, 8);
+    addEdge(2,5,10);
+    addEdge(5,7,10);
+    addEdge(6,8,10);
+    // addEdge(0,6,8);
+    // display();
+    // cout << endl;
+}
